80bb6a5042abecd3c76c5735f38207042acf79d0
SaSaSaBo <sasasabo14@gmail.com>
2024-01-23 12:46:11 +0300

    homepage pt2

--- containers/home/index.js
+++ containers/home/index.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Movies from '@/mocks/movies.json';
 import Genres from '@/mocks/genres.json';
 import Categories from '@/components/categories';
+import MovieSection from '@/components/movie-section';
 
 
 function HomeContainer() {
@@ -10,6 +11,9 @@ function HomeContainer() {
     <div>
         <FeaturedMovie movie={Movies.results[0]}/>
         <Categories categories={Genres.genres.slice(0, 5)}/>
+        <MovieSection title='Popular Movies' movies={Movies.results.slice(0, 8)}/> 
+        <br/>
+        <MovieSection title='Favorite Movies' movies={Movies.results.slice(8, 14)}/>
     </div>
   )
 }
